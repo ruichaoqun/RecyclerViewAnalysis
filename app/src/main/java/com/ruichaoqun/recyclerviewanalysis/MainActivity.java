e33341bb6ba66be53dab21815d3c43887593e1ff
ruichaoqun <ruicq@zgjzd.cn>
2019-02-25 18:12:58 +0800

    Initial commit

---
+++ app/src/main/java/com/ruichaoqun/recyclerviewanalysis/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ruichaoqun.recyclerviewanalysis;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
